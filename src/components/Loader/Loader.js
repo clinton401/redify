253f928ff9f482af7c2b7457e1274db8a5b18eb4
clinton <clintonphillips464@gmail.com>
2023-08-23 13:15:26 +0100

    added-some-features

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import loadcss from './Loader.module.css';
 import { IconContext } from "react-icons";
 import { FaRedditAlien } from "react-icons/fa";
+import { BeatLoader } from "react-spinners";
 
 function Loader() {
+  const [loading, setloading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setloading(false)
+    }, 2800);
+  }, [])
+  const CSSProperties = {
+    // position: "absolute",
+    // top: "40%",
+    marginTop: '20px',
+    marginRight: '10px',
+    // border: '1px solid black'
+    
+  };
   return (
       <div className={loadcss.load}>
-          <div>
+      <div>
+       
+        
           <IconContext.Provider value={{ className: "reddit-icon red" }}>
               <FaRedditAlien />
           </IconContext.Provider>
-              
+        {loading ? <BeatLoader
+          size={15}
+          color={"white"}
+
+          cssOverride={CSSProperties}
+        /> : null}
           </div>
     </div>
   )
